b4f72a47556fe11a9efa5c8c8968355901c03e15
David Bille <d_bille@yahoo.com>
2020-05-23 20:59:52 -0500

    able to display buttons for each individual method display

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ import { arrayMethods } from './data.js'
 let methodBtn = document.querySelectorAll('#method-btn')
 
 arrayMethods.forEach(item=>{
-  console.log(item)
+  let body = document.querySelector('.btn-display-field')
+  let btn = document.createElement('button')
+  btn.textContent = item.name.split('.')[1];
+  body.appendChild(btn)
 })
 
 let h2 = document.querySelector('.method-name-h2')
