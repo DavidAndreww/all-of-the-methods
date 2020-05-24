caa54ff3429fc2e0e905274355e2919e9a35d178
David Bille <d_bille@yahoo.com>
2020-05-23 21:46:50 -0500

    cool beans

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-import { data } from './data.js'
+import { data } from "./data.js";
 
-let methodBtn = document.querySelectorAll('#method-btn')
+let methodBtn = document.querySelectorAll("#method-btn");
 
-methodBtn.forEach(button => button.addEventListener('click', ()=>{
-  let array = data[button.name]
-  methodButtonGenerator(array)
-}))
-
-function loadMethodToDom(object){
-  let h2 = document.querySelector('.method-name-h2')
-  let p = document.querySelector('method-description-p')
-  h2.innerHTML = object.name;
-  p.textContent = object.description;
-
-}
-
-
-
-
-function methodButtonGenerator(array){
-  let selectedArray = array;
-  array.forEach(item=>{
-    let body = document.querySelector('.btn-display-field')
-    let btn = document.createElement('button')
-    btn.addEventListener('click', ()=> {
-      let arrayName = item.name.split('.')[1]
-      let [found] = selectedArray.filter(item => item.name.split('.')[1] === arrayName)
-      console.log(found)
-      loadMethodToDom(found)
-    })
-    btn.className = item.name.split('.')[1]
-    btn.textContent = item.name.split('.')[1];
-    body.appendChild(btn)
+methodBtn.forEach((button) =>
+  button.addEventListener("click", () => {
+    let array = data[button.name];
+    methodButtonGenerator(array);
   })
+);
+
+function loadMethodToDom(object) {
+  console.log(object.description);
+  let h2 = document.querySelector(".method-name-h2");
+  let p = document.querySelector(".method-description-p");
+  h2.innerHTML = object.name;
+  p.innerHTML = object.description;
 }
 
-
-
+function methodButtonGenerator(array) {
+  let selectedArray = array;
+  array.forEach((item) => {
+    let body = document.querySelector(".btn-display-field");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", () => {
+      let arrayName = item.name.split(".")[1];
+      let [found] = array.filter(
+        (item) => item.name.split(".")[1] === arrayName
+      );
+      console.log(found);
+      loadMethodToDom(found);
+    });
+    btn.className = item.name.split(".")[1];
+    btn.textContent = item.name.split(".")[1];
+    body.appendChild(btn);
+  });
+}
