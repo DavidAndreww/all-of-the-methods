cdb75b85a800ffba517ee746705d0b23f9c43ad5
David Bille <d_bille@yahoo.com>
2020-05-23 19:48:56 -0500

    adding functionality to click buttons and identify each individual button

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('All of the methods!')
+let methodBtn = document.querySelectorAll('#method-btn')
+// console.log(methodBtn)
+methodBtn.forEach(button => button.addEventListener('click', ()=>{
+  console.log('clickedMe!: ', button.name)
+}))
