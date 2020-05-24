410e4c740bb47874d2d879129d0c0cff98c01c41
David Bille <d_bille@yahoo.com>
2020-05-23 21:44:21 -0500

    able to display specific data to array button that was clicked

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 import { data } from './data.js'
 
 let methodBtn = document.querySelectorAll('#method-btn')
+
 methodBtn.forEach(button => button.addEventListener('click', ()=>{
-  console.log(button.name)
+  let array = data[button.name]
+  methodButtonGenerator(array)
 }))
 
+function loadMethodToDom(object){
+  let h2 = document.querySelector('.method-name-h2')
+  let p = document.querySelector('method-description-p')
+  h2.innerHTML = object.name;
+  p.textContent = object.description;
 
-const buttonCreator = function (array){
+}
+
+
+
+
+function methodButtonGenerator(array){
+  let selectedArray = array;
   array.forEach(item=>{
     let body = document.querySelector('.btn-display-field')
     let btn = document.createElement('button')
+    btn.addEventListener('click', ()=> {
+      let arrayName = item.name.split('.')[1]
+      let [found] = selectedArray.filter(item => item.name.split('.')[1] === arrayName)
+      console.log(found)
+      loadMethodToDom(found)
+    })
+    btn.className = item.name.split('.')[1]
     btn.textContent = item.name.split('.')[1];
     body.appendChild(btn)
   })
 }
 
-// arrayMethods.forEach(item=>{
-//   let body = document.querySelector('.btn-display-field')
-//   let btn = document.createElement('button')
-//   btn.textContent = item.name.split('.')[1];
-//   body.appendChild(btn)
-// })
-
-let h2 = document.querySelector('.method-name-h2')
 
 
