fdd089fa90d846da86cbb5476c9a7b6c0b28ca09
David Bille <d_bille@yahoo.com>
2020-06-05 08:22:34 -0500

    successfully refactored

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ primarybuttons.forEach((button) => {
   const arrayName = button.name;
   let methodsToLoad = data[arrayName];
   button.addEventListener("click", () => {
-    secondaryBtnGenerator(methodsToLoad)
-    addEventToSecondaryButtons()
+    secondaryBtnGenerator(methodsToLoad);
+    addEventToSecondaryButtons(methodsToLoad);
   });
 });
 
@@ -24,72 +24,31 @@ function secondaryBtnGenerator(list) {
   const array = list;
   // let secondaryButtons = [];
   const displayDiv = document.querySelector(".btn-display-field");
-  // adds name, class, and textContent to each button 
-    array.forEach((method) => {
+  // adds name, class, and textContent to each button
+  array.forEach((method) => {
     const btn = document.createElement("button");
     btn.name = method.name;
-    btn.textContent = method.name
-    btn.className = 'individual-method-btn'
-    displayDiv.appendChild(btn)
+    btn.textContent = method.name;
+    btn.className = "individual-method-btn";
+    displayDiv.appendChild(btn);
   });
-
 }
 
-function addEventToSecondaryButtons(){
-  const secondaryButtons = document.querySelectorAll('.individual-method-btn')
-  // console.log(secondaryButtons)
-  secondaryButtons.forEach(button => {
-    button.addEventListener('click', () => loadToDom(button.name)) 
-  })
-  
+function addEventToSecondaryButtons(methods) {
+  const secondaryButtons = document.querySelectorAll(".individual-method-btn");
+  secondaryButtons.forEach((button) => {
+    let found = methods.find((method) => method.name === button.name);
+    button.addEventListener("click", () => loadToDom(found));
+  });
 }
 
-
-
-function loadToDom(obj){
-  console.log(obj)
+function loadToDom(obj) {
+  console.log(obj);
+  let h2 = document.querySelector('.method-name-h2');
+  let p = document.querySelector('.method-description-p');
+  let textArea = document.querySelector('.text-area')
+  h2.innerHTML = obj.name;
+  p.innerHTML = obj.description;
+  textArea.innerHTML = `${obj.editor}`;
 }
 
-// let h2 = document.querySelector('.method-name-h2');
-// let p = document.querySelector('.method-description-p');
-// let textArea = document.querySelector('.text-area')
-
-
-
-// // adds event listener
-// methodLoaderButton.forEach((button) =>
-//   button.addEventListener("click", () => {
-//     let array = data[button.name];
-//     methodButtonGenerator(array);
-//   })
-// );
-
-// function loadMethodToDom(object) {
-
-//   let h2 = document.querySelector(".method-name-h2");
-//   let p = document.querySelector(".method-description-p");
-//   let textArea = document.querySelector(".text-area");
-//   h2.innerHTML = object.name;
-//   p.innerHTML = object.description;
-//   textArea.innerHTML = `${object.editor}`;
-
-// }
-
-// function methodButtonGenerator(array) {
-//   let selectedArray = array;
-//   array.forEach((item) => {
-//     let body = document.querySelector(".btn-display-field");
-//     let btn = document.createElement("button");
-//     btn.addEventListener("click", () => {
-//       let arrayName = item.name.split(".")[1];
-//       let [found] = selectedArray.filter(
-//         (item) => item.name.split(".")[1] === arrayName
-//       );
-//       console.log(found);
-//       loadMethodToDom(found);
-//     });
-//     btn.className = "individual-method-btn";
-//     btn.textContent = item.name.split(".")[1];
-//     body.appendChild(btn);
-//   });
-// }
