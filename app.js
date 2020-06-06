7d51ae41d5d9097d06e81277dd2938385b4432fd
David Bille <d_bille@yahoo.com>
2020-06-06 15:58:07 -0500

    changed a few variable/function names and added comments to app.js

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
 import { data } from "./data.js";
 
-// identify each hard coded button from the HTML document
-// add event listener to each button that listens for a click
-// on listener click, identify that button and retrieve data from corresponding array
-// create a unique button for each object in that array of data
-// for each button, create another on click event listener
-// on click must take a unique button and find corresponding object in the array
-// load buttons to the page to be displayed
-// clear current list of buttons from screen when new button is clicked
-
 const primarybuttons = document.querySelectorAll("#method-btn");
 
-// add click event listener to each primary button. Event will generate secondary buttons, as well as add event listeners to those buttons
+// add onClick event listener to each primary button, that calls secondaryBtnGenerator()
 primarybuttons.forEach((button) => {
-  const arrayName = button.name;
-  let methodsToLoad = data[arrayName];
+  // identifies methods to load from corresponding button name attribute
+  const arrOfSelectedMethods = button.name;
+  let methodsToLoad = data[arrOfSelectedMethods];
+  // declares event listener
   button.addEventListener("click", () => {
-    const displayDiv = document.querySelector(".btn-display-field");
-    displayDiv.innerHTML = '';
+    // clears current display of secondary buttons
+    const secondaryBtnDisplayDiv = document.querySelector(".btn-display-field");
+    secondaryBtnDisplayDiv.innerHTML = '';
+    // passes array of selected method objects to secondaryBtnGenerator()
     secondaryBtnGenerator(methodsToLoad);
   });
 });
 
-function secondaryBtnGenerator(list) {
-  const array = list;
-  // let secondaryButtons = [];
-  const displayDiv = document.querySelector(".btn-display-field");
-  // adds name, class, textContent and eventListener to each button
-  array.forEach((method) => {
+// takes an array of objects as parameter. For each object, creates button with name, class, textContent and event listener
+function secondaryBtnGenerator(array) {
+  const arr = array;
+  const secondaryBtnDisplayDiv = document.querySelector(".btn-display-field");
+  arr.forEach((method) => {
     const btn = document.createElement("button");
     btn.name = method.name;
     btn.textContent = method.name.split('.')[1];
     btn.className = "individual-method-btn";
-    btn.addEventListener('click',() => loadMethodToDOM(method))
-    displayDiv.appendChild(btn);
+    btn.addEventListener('click',() => renderMethodToDOM(method))
+    secondaryBtnDisplayDiv.appendChild(btn);
   });
 }
 
-function loadMethodToDOM(obj) {
+// takes single object as parameter. Renders appropriate data to the DOM
+function renderMethodToDOM(obj) {
   let h2 = document.querySelector('.method-name-h2');
   let p = document.querySelector('.method-description-p');
   let textArea = document.querySelector('.text-area');
