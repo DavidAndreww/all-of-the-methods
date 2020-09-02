2f148f7b0af3b83212e7f097a05ecae1cde11111
unknown <d_bille@yahoo.com>
2020-09-02 18:06:08 -0500

    created functionality to display message for methos which do not have information yet

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import { data } from "./data.js";
-let mirrorArea = document.querySelector('.text-area')
+let mirrorArea = document.querySelector(".text-area");
 let editor = CodeMirror.fromTextArea(mirrorArea, {
   lineNumbers: true,
   mode: "javascript",
-  theme: "duotone-light"
-})
+  theme: "duotone-light",
+});
 
 const primarybuttons = document.querySelectorAll(".primary-btn");
 // let mirror = document.querySelector('.method-code-editor')
-
 
 // add onClick event listener to each primary button, that calls secondaryBtnGenerator()
 primarybuttons.forEach((button) => {
@@ -19,7 +18,7 @@ primarybuttons.forEach((button) => {
   button.addEventListener("click", () => {
     // clears current display of secondary buttons
     const secondaryBtnDisplayDiv = document.querySelector(".btn-display-field");
-    secondaryBtnDisplayDiv.innerHTML = '';
+    secondaryBtnDisplayDiv.innerHTML = "";
     // passes array of selected method objects to secondaryBtnGenerator()
     secondaryBtnGenerator(methodsToLoad);
   });
@@ -27,40 +26,49 @@ primarybuttons.forEach((button) => {
 
 // takes an array of objects as parameter. For each object, creates button with name, class, textContent and event listener
 function secondaryBtnGenerator(array) {
+  console.log("methods", array);
   const arr = array;
   const secondaryBtnDisplayDiv = document.querySelector(".btn-display-field");
-  let n = 1
+
+  if (array.length === 0) {
+    const notification = document.createElement("div");
+    notification.innerHTML = 'Not Available Yet...';
+    notification.className = 'notification-div'
+    secondaryBtnDisplayDiv.appendChild(notification)
+  }
+
   arr.forEach((method) => {
     const btn = document.createElement("button");
     btn.name = method.name;
-    btn.textContent = method.name.split('.')[1];
+    btn.textContent = method.name.split(".")[1];
     btn.className = `individual-method-btn`;
-    btn.addEventListener('click',() => renderMethodToDOM(method))
+    btn.addEventListener("click", () => renderMethodToDOM(method));
     secondaryBtnDisplayDiv.appendChild(btn);
   });
 }
 
 // takes single object as parameter. Renders appropriate data to the DOM
 function renderMethodToDOM(obj) {
-  let h2 = document.querySelector('.method-name-h2');
-  let p = document.querySelector('.method-description-p');
-  let textArea = document.querySelector('.text-area');
+  let h2 = document.querySelector(".method-name-h2");
+  let p = document.querySelector(".method-description-p");
+  let textArea = document.querySelector(".text-area");
   h2.innerHTML = obj.name;
   p.innerHTML = obj.description;
-  editor.setValue(obj.editor) //codemirror method to set value of textarea
+  editor.setValue(obj.editor); //codemirror method to set value of textarea
 }
 
-const codeDisplayField = document.querySelector('#code-execution-field')
-const submitBtn = document.querySelector('#submit')
-submitBtn.addEventListener('click',function(){
-  let codeToExecute = editor.getValue()
-  console.log(codeToExecute)
+const codeDisplayField = document.querySelector("#code-execution-field");
+const submitBtn = document.querySelector("#submit");
+submitBtn.addEventListener("click", function () {
+  let codeToExecute = editor.getValue();
+  console.log("code in editor: ", codeToExecute);
 
   //TODO create functionality to run code within mirror and provide console.log to user
-  let J = new Function(codeToExecute)
-  console.log('J function', J)
-  // let console = editor.lineCount()-1;
-  // console.log(editor.getLine(console))
-  codeDisplayField.innerHTML = J()
-    
-})
+  // let solution = new Function(codeToExecute)
+  // console.log(solution())
+  // console.log('function to run: ', solution)
+
+  // let result = eval(solution())
+  // console.log('result:', result)
+  // codeDisplayField.innerHTML = solution()
+});
